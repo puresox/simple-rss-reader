11562b2f4dae0f7dbf0382ba73dea52d1163ec81
puresox <puresox@163.com>
2017-12-10 19:14:39 +0800

    fix

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,18 @@
-$(function(){
-    $("#login").click(function(){
-        //alert("dd");
-        if($("#account").val()==null||$("#account").val()==""){
-            alert("请输入账号");
-            return false;
-        }
-        if($("#password").val()==null||$("#password").val()==""){
-            alert("请输入密码");
-            return false;
-        }
-        
-        $("#loginFrom").submit();
-    });
-    $("#goSign").click(function(){
-        location.href="sign_up.html";
-    });
-  }); 
+$(() => {
+  $('#login').click(() => {
+    // alert("dd");
+    if ($('#account').val() == null || $('#account').val() == '') {
+      alert('请输入账号');
+      return false;
+    }
+    if ($('#password').val() == null || $('#password').val() == '') {
+      alert('请输入密码');
+      return false;
+    }
+
+    $('#loginFrom').submit();
+  });
+  $('#goSign').click(() => {
+    location.href = '/signup';
+  });
+});
